7e9f295bfa1b5a910325d7c4a5b14f1543156ede
Sebastien ROHAUT <sebastien.rohaut@free.fr>
2013-04-12 23:17:20 +0200

    Add file header. Define LOCALE and TIMEZONE for time functions

--- include/config.inc.php
+++ include/config.inc.php
@@ -47,3 +47,6 @@ $vrrp_instance_dictionnary = array ('name', 'use_vmac', 'native_ipv6', 'interfac
 $ip_address_dictionnary = array ('ip', 'mask', 'broadcast', 'dev', 'scope', 'label', 'is_gateway', 'is_disabled', 'cluster_id', 'virtual_router_id' );
 $vrrp_sync_group_dictionnary = array ('name', 'notify_master', 'notify_backup', 'notify_fault', 'notify', 'smtp_alert', 'cluster_id');
 $vrrp_script_dictionnary = array('name','script','interval','weight','fall','rise');
+
+setlocale(LC_ALL, LOCALE);
+date_default_timezone_set(TIMEZONE);
